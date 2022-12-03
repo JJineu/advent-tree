43bb712d2922c69e07da7dae3be4eb450290058e
joice_c <hyunjin.kim@crytoparadise.io>
2022-12-03 18:32:51 +0900

    feat:login,register

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
 import './App.css';
-import SojuTree from './component/SojuTree.js';
-import NavBar from './component/NavBar.js';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Auth from './hoc/auth';
 
 /* Components */
-import MainPage from './components/MainPage';
-import LoginPage from './components/LoginPage';
+// import MainPage from './component/MainPage';
+import LoginPage from './component/LoginPage';
+import RegisterPage from './component/RegisterPage';
 
 function App() {
-  // const AuthMainPage = Auth(MainPage, false);
-  // const AuthLoginPage = Auth(LoginPage, false);
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/login" component={LoginPage} />
-          {/* <Route exact path="/" element={<AuthMainPage />} />
-        <Route exact path="/login" element={<AuthLoginPage />} /> */}
+          {/* <Route exact path="/" element={<MainPage />} /> */}
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<RegisterPage />} />
         </Routes>
       </Router>
     </div>
